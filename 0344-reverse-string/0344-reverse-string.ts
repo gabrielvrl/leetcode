e783bf535f63f2400cb6bf4e060b49c9e2df36f5
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2023-07-06 14:25:43 -0300

    Time: 157 ms (5.41%), Space: 53.7 MB (5.01%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.ts
@@ -0,0 +1,13 @@
+/**
+ Do not return anything, modify s in-place instead.
+ */
+function reverseString(s: string[]): void {
+    const stringReversed = s.reverse()
+    // let stringReversed: string[] = [];
+    
+    // for(let i = s.length -1; i >= 0; i--){
+    //     stringReversed.push(s[i]);
+    // }
+    
+    console.log(stringReversed)
+};
