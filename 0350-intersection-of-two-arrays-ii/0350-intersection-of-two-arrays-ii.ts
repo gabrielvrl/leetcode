22adfffc9564f9af8ecddc9e58229beed9f38afa
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2023-07-09 18:11:47 -0300

    Time: 69 ms (55.26%), Space: 44.9 MB (73.03%) - LeetHub

---
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.ts
@@ -0,0 +1,25 @@
+function intersect(nums1: number[], nums2: number[]): number[] {
+    
+    nums1.sort((a,b) => a-b)
+    nums2.sort((a,b) => a-b)
+
+    const result: number[] = [];
+
+    let i = 0;
+    let j = 0;
+
+    while (i < nums1.length && j < nums2.length) {
+        if (nums1[i] < nums2[j]) {
+            i++;
+        } else if (nums1[i] > nums2[j]) {
+            j++;
+        } else {
+            result.push(nums1[i]);
+            i++;
+            j++;
+        }
+    }
+
+    return result;
+    
+};
