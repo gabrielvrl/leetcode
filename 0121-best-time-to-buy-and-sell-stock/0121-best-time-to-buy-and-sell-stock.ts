88db25428fdd626674a5d416338518f20cf47f3f
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2023-07-10 16:37:00 -0300

    Time: 78 ms (93.49%), Space: 51.6 MB (89.23%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.ts
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.ts
@@ -1,24 +1,38 @@
 function maxProfit(prices: number[]): number {
 
-    let buy = prices[0];
-    let sell = 0;
 
-    let potentialMax = 0;
+    // let buy = prices[0];
+    // let sell = 0;
+
+    // let potentialMax = 0;
+
+    // // for(let i = 0; i < prices.length; i++){
+    // //     for(let j = i+1; j < prices.length; j++){
+    // //         if(prices[j] - prices[i] > potentialMax){
+    // //             potentialMax = prices[j] - prices[i]
+    // //         }
+    // //     }
+    // // }
 
     // for(let i = 0; i < prices.length; i++){
-    //     for(let j = i+1; j < prices.length; j++){
-    //         if(prices[j] - prices[i] > potentialMax){
-    //             potentialMax = prices[j] - prices[i]
-    //         }
-    //     }
+    //     buy = Math.min(buy , prices[i]);
+    //     sell = Math.max(sell, prices[i] - buy);
     // }
 
+
+
+    // return sell;
+
+    let minPrice = prices[0];
+    let maxProfit = 0;
+
     for(let i = 0; i < prices.length; i++){
-        buy = Math.min(buy , prices[i]);
-        sell = Math.max(sell, prices[i] - buy);
+        if(prices[i] < minPrice) {
+            minPrice = prices[i]
+        } else if(prices[i] - minPrice > maxProfit){
+            maxProfit = prices[i] - minPrice
+        }
     }
 
-
-
-    return sell;
+    return maxProfit
 };
