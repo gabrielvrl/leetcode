be45daf510da22ffd030d21e66b12ea86bbdc1b6
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2024-01-17 20:42:18 -0300

    Time: 71 ms (25.14%), Space: 45.3 MB (30.73%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.ts
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.ts
@@ -1,48 +1,23 @@
-function maxNumberOfBalloons(text: string): number {
-    const hashMap = {}
-    let result = 0;
-    
-    for(let i = 0; i < text.length; i++) {
-        const element = text[i]
-        if(hashMap[element] === undefined) {
-            hashMap[element] = 1
-        } else {
-            hashMap[element]++;
-        }
+/**
+ * @param {string} text
+ * @return {number}
+ */
+var maxNumberOfBalloons = function(text) {
+    const obj = {}
+    let balloonCount = 0;
+    for (let c of text) {
+        obj[c] = (obj[c] || 0) + 1;
     }
-    
-    // balloon
-    // b: 1
-    // a: 1
-    // l: 2
-    // o: 2
-    // n: 1
-    // ou múltiplos
-    
-    const maximumPossibleBallons = hashMap["b"]
-    
-    for(let i = 0; i < maximumPossibleBallons; i++){
-        if(hashMap["b"] >= 1 && hashMap["a"] >= 1 && hashMap["l"] >= 2 && hashMap["o"] >= 2 && hashMap["n"] >= 1){
-            result++;
-            hashMap["b"] = hashMap["b"] - 1
-            hashMap["a"] = hashMap["a"] - 1
-            hashMap["l"] = hashMap["l"] - 2
-            hashMap["o"] = hashMap["o"] - 2
-            hashMap["n"] = hashMap["n"] - 1
-        }
+
+    while (obj['a'] > 0 && obj['b'] > 0 && obj['l'] > 1 && obj['n'] > 0 && obj['o'] > 1) {
+        balloonCount++;
+
+        obj['a']--;
+        obj['b']--;
+        obj['l'] -= 2;
+        obj['n']--;
+        obj['o'] -= 2;
     }
-    
-    return result
+
+    return balloonCount;
 };
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
