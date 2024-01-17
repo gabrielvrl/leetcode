128dce0b3917a44f5d5f6af0b9d9b2e64ebbd675
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2024-01-17 20:36:21 -0300

    Time: 142 ms (5.03%), Space: 51 MB (5.03%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.ts
@@ -0,0 +1,54 @@
+function maxNumberOfBalloons(text: string): number {
+    const hashMap = {}
+    let result = 0;
+    
+    for(let i = 0; i < text.length; i++) {
+        const element = text[i]
+        if(hashMap[element] === undefined) {
+            hashMap[element] = 1
+        } else {
+            hashMap[element]++;
+        }
+    }
+    
+    // balloon
+    // b: 1
+    // a: 1
+    // l: 2
+    // o: 2
+    // n: 1
+    // ou múltiplos
+    console.log(hashMap)
+    const maximumPossibleBallons = hashMap["b"]
+    console.log(maximumPossibleBallons)
+    let ballonCondition;
+    
+    for(let i = 0; i < maximumPossibleBallons; i++){
+        console.log(i)
+        console.log(ballonCondition)
+        ballonCondition = hashMap["b"] >= 1 && hashMap["a"] >= 1 && hashMap["l"] >= 2 && hashMap["o"] >= 2 && hashMap["n"] >= 1
+        if(ballonCondition){
+            result++;
+            hashMap["b"] = hashMap["b"] - 1
+            hashMap["a"] = hashMap["a"] - 1
+            hashMap["l"] = hashMap["l"] - 2
+            hashMap["o"] = hashMap["o"] - 2
+            hashMap["n"] = hashMap["n"] - 1
+        }
+        console.log(hashMap["l"])
+    }
+    
+    return result
+};
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
